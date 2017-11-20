4ef7b564da370eeffe70f17a87f83c45c56ca2e6
Wojciech Grazawski <wojciech.grazawski@wp.pl>
2017-11-20 21:28:22 +0100

    testy automatyczne

--- app/src/androidTest/java/org/pl/android/navimee/MainActivityTest.java
+++ app/src/androidTest/java/org/pl/android/navimee/MainActivityTest.java
@@ -1,19 +1,25 @@
 package org.pl.android.navimee;
 
 
+import android.os.Build;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.pl.android.navimee.ui.main.MainActivity;
 
+import static android.os.SystemClock.sleep;
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -23,16 +29,21 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
 
+    @Rule public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+
+
 
     @Test
     public void clickBottomBar() {
         // Type text and then press the button.
+        sleep(1000);
         onView(withId(R.id.tab_events))
               .perform(click());
-
+        sleep(1000);
         onView(withId(R.id.tab_day_schedule))
                 .perform(click());
-
+        sleep(1000);
         onView(withId(R.id.tab_hotspot))
                 .perform(click());
     }
