c0016278bb55236171403c012677f698e06c6b21
wojciech.grazawski <wojciech.grazawski@wp.pl>
2017-12-28 19:11:33 +0100

    zmiany feeback cd

--- app/src/main/java/org/pl/android/navimee/notifications/MyFirebaseInstanceIDService.java
+++ app/src/main/java/org/pl/android/navimee/notifications/MyFirebaseInstanceIDService.java
@@ -84,10 +84,12 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
         // Get a reference to the restaurants collection
         if(dataManager.getFirebaseService().getFirebaseAuth().getCurrentUser() != null) {
             String userId = dataManager.getFirebaseService().getFirebaseAuth().getCurrentUser().getUid();
+            String name = dataManager.getFirebaseService().getFirebaseAuth().getCurrentUser().getDisplayName();
             Map<String, Object> user = new HashMap<>();
             user.put("token", token);
             user.put("email", dataManager.getFirebaseService().getFirebaseAuth().getCurrentUser().getEmail());
             user.put("id",userId);
+            user.put("name",name);
             dataManager.getFirebaseService().getFirebaseFirestore().collection("USERS").document(userId).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void aVoid) {
