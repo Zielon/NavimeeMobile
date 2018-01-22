b4244c731f066d6197546f1307b43d5ddd1d8e90
Wojciech Grazawski <wojciech.grazawski@wp.pl>
2018-01-22 21:00:27 +0100

    poprawka usuwanie grupy

--- app/src/main/java/org/pl/android/drively/ui/chat/group/GroupPresenter.java
+++ app/src/main/java/org/pl/android/drively/ui/chat/group/GroupPresenter.java
@@ -10,12 +10,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.WriteBatch;
 
 import org.pl.android.drively.data.DataManager;
 import org.pl.android.drively.data.model.chat.Group;
@@ -27,6 +32,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import javax.inject.Inject;
 
@@ -128,6 +136,8 @@ public class GroupPresenter extends BasePresenter<GroupMvpView> {
 
 
     public void deleteGroup(Group group) {
+        deleteCollection( mDataManager.getFirebaseService().getFirebaseFirestore().collection("GROUP")
+                .document(group.id).collection("MEMBERS"),1000, Executors.newSingleThreadExecutor());
         mDataManager.getFirebaseService().getFirebaseFirestore().collection("GROUP").document(group.id)
                     .delete()
                     .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -147,6 +157,54 @@ public class GroupPresenter extends BasePresenter<GroupMvpView> {
 
     }
 
+
+
+    private Task<Void> deleteCollection(final CollectionReference collection,
+                                        final int batchSize,
+                                        Executor executor) {
+        // Perform the delete operation on the provided Executor, which allows us to use
+        // simpler synchronous logic without blocking the main thread.
+        return Tasks.call(executor, new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                // Get the first batch of documents in the collection
+                Query query = collection.orderBy(FieldPath.documentId()).limit(batchSize);
+
+                // Get a list of deleted documents
+                List<DocumentSnapshot> deleted = deleteQueryBatch(query);
+
+                // While the deleted documents in the last batch indicate that there
+                // may still be more documents in the collection, page down to the
+                // next batch and delete again
+                while (deleted.size() >= batchSize) {
+                    // Move the query cursor to start after the last doc in the batch
+                    DocumentSnapshot last = deleted.get(deleted.size() - 1);
+                    query = collection.orderBy(FieldPath.documentId())
+                            .startAfter(last.getId())
+                            .limit(batchSize);
+
+                    deleted = deleteQueryBatch(query);
+                }
+
+                return null;
+            }
+        });
+
+        // Logger.LOG(new Log(LogTypes.DELETION, collection.getPath(), deletedAll));
+    }
+
+
+    private List<DocumentSnapshot> deleteQueryBatch(final Query query) throws Exception {
+        QuerySnapshot querySnapshot = Tasks.await(query.get());
+
+        WriteBatch batch = query.getFirestore().batch();
+        for (DocumentSnapshot snapshot : querySnapshot) {
+            batch.delete(snapshot.getReference());
+        }
+        Tasks.await(batch.commit());
+        return querySnapshot.getDocuments();
+    }
+
     public void deleteGroupReference(int index, Group group) {
         mDataManager.getFirebaseService().getFirebaseFirestore().collection("USERS")
                 .document(group.member.get(index))
@@ -156,6 +214,12 @@ public class GroupPresenter extends BasePresenter<GroupMvpView> {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
+                            if(task.getResult().size() == 0) {
+                                if (getMvpView() != null) {
+                                    getMvpView().onFailureGroupReference();
+                                    return;
+                                }
+                            }
                             for (DocumentSnapshot document : task.getResult()) {
                                 Timber.d( document.getId() + " => " + document.getData());
                                 mDataManager.getFirebaseService().getFirebaseFirestore().collection("USERS").document(group.member.get(index)).collection("GROUP")
