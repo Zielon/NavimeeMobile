02781b03964133347bd31617609565d3072414b5
Milszym <milszym@gmail.com>
2018-03-01 22:23:50 +0100

    group -> room

--- app/src/main/java/org/pl/android/drively/util/ChatUtils.java
+++ app/src/main/java/org/pl/android/drively/util/ChatUtils.java
@@ -5,9 +5,9 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 
 import org.pl.android.drively.R;
-import org.pl.android.drively.data.model.RoomMember;
-import org.pl.android.drively.data.model.chat.Group;
 import org.pl.android.drively.data.model.chat.ListFriend;
+import org.pl.android.drively.data.model.chat.Room;
+import org.pl.android.drively.data.model.chat.RoomMember;
 import org.pl.android.drively.ui.chat.chatview.ChatViewActivity;
 import org.pl.android.drively.ui.chat.data.FriendDB;
 import org.pl.android.drively.ui.chat.data.GroupDB;
@@ -29,8 +29,8 @@ public class ChatUtils {
         intent.putExtra(Const.INTENT_KEY_CHAT_FRIEND, groupName);
         ArrayList<CharSequence> idFriend = new ArrayList<>();
         ChatViewActivity.bitmapAvataFriend = new HashMap<>();
-        ArrayList<Group> listGroup = GroupDB.getInstance(context).getListGroups();
-        Group group = StreamSupport.stream(listGroup)
+        ArrayList<Room> listGroup = GroupDB.getInstance(context).getListGroups();
+        Room group = StreamSupport.stream(listGroup)
                 .filter(singleGroup -> singleGroup.getName().equals(groupName))
                 .findFirst().get();
         for (RoomMember member : group.getMembers()) {
@@ -47,7 +47,7 @@ public class ChatUtils {
         }
 
         intent.putCharSequenceArrayListExtra(Const.INTENT_KEY_CHAT_ID, idFriend);
-        intent.putExtra(Const.INTENT_KEY_CHAT_ROOM_ID, group.id);
+        intent.putExtra(Const.INTENT_KEY_CHAT_ROOM_ID, group.getId());
         intent.putExtra(Const.INTENT_KEY_IS_GROUP_CHAT, true);
 
         return intent;
