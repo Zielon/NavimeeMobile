f8afa0f4753c7772446e8efa0c9d5c0f482a5a66
Milszym <milszym@gmail.com>
2018-03-04 12:00:41 +0100

    startowanie lokalizacji po zmianie aplikacji

--- app/src/main/java/org/pl/android/drively/service/GeolocationUpdateService.java
+++ app/src/main/java/org/pl/android/drively/service/GeolocationUpdateService.java
@@ -167,6 +167,7 @@ public class GeolocationUpdateService extends Service {
     @Subscribe
     public void onUserCompanyChanged(UserCompanyChanged userCompanyChanged) {
         USER_COMPANY = userCompanyChanged.getUserCompany();
+        startLocationUpdates();
     }
 
 }
