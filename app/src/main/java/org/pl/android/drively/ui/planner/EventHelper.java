6104d85fad060d12a4cdf6f92449f49fd03ee8bf
Milszym <milszym@gmail.com>
2018-03-05 22:37:08 +0100

    poprawka w filtrowaniu evento w planie dnia oraz poprawka przy pierwszej instalacji z preferencjami

--- app/src/main/java/org/pl/android/drively/ui/planner/EventHelper.java
+++ app/src/main/java/org/pl/android/drively/ui/planner/EventHelper.java
@@ -2,7 +2,6 @@ package org.pl.android.drively.ui.planner;
 
 import org.pl.android.drively.data.model.Event;
 
-import java.util.Date;
 import java.util.List;
 
 import java8.util.stream.Collectors;
@@ -11,8 +10,6 @@ import java8.util.stream.StreamSupport;
 public class EventHelper {
 
     public static List<Event> sortEvents(List<Event> eventList) {
-        Date today = new Date();
-        today.setMinutes(today.getMinutes() + 30);
         return StreamSupport.stream(eventList)
                 .sorted((event1, event2) -> Integer.valueOf(event1.getRank()).compareTo((event2.getRank())))
                 .sorted((event1, event2) -> event1.getStartTime().compareTo(event2.getStartTime()))
